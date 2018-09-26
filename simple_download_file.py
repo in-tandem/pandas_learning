fbbf4a8c01e26905ca3c5bc852cf9b5c97faadab
somak <somaktukai@gmail.com>
2018-09-26 19:30:32 +0530

    panda learning

---
+++ simple_download_file.py
@@ -0,0 +1,12 @@
+import pandas as panda
+
+remote_location = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+
+data = panda.read_csv(remote_location)
+
+print(data.tail())
+print(data[35:60])
+print(data.iloc[35:60, 1].values)
+print(list(data.iloc[35:60, 1].values))
+print(type(list(data.iloc[35:60, 1].values)))
+print(type(data.iloc[35:60, 1].values))
